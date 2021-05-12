820409c4916a569199645331c5f4bfbccf2cb3bf
hyojinsim <hyojinsim90@gmail.com>
2021-05-13 03:45:01 +0900

    test

--- src/test/java/MariaDBTest.java
+++ src/test/java/MariaDBTest.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class MariaDBTest {
 
-    @Test
+  /*  @Test
     public void test() throws Exception {
         Class.forName("org.mariadb.jdbc.Driver"); // 마리아DB
         // Class.forName("com.mysql.jdbc.Driver")
@@ -13,5 +13,5 @@ public class MariaDBTest {
         Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/timeline", "root", "maria1234"); // 마리아 DB
 
         System.out.println(con);
-    }
+    }*/
 }
