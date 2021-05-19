0ba57ddbf30de889204d4f03e8222350809fd277
hyojinsim <hyojinsim90@gmail.com>
2021-05-19 14:05:56 +0900

    modified properties

--- src/test/java/MariaDBTest.java
+++ src/test/java/MariaDBTest.java
@@ -5,14 +5,16 @@ import java.sql.DriverManager;
 
 public class MariaDBTest {
 
-   /*@Test
+   @Test
     public void test() throws Exception {
        Class.forName("org.mariadb.jdbc.Driver"); // 마리아DB
        // Class.forName("com.mysql.jdbc.Driver")
 
-       Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/timeline", "root", "maria1234"); // 마리아 DB
+//       Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/timeline", "root", "maria1234"); // 마리아 DB
+
+       Connection con = DriverManager.getConnection("jdbc:mariadb://timeline.cmo8b1bgzk71.ap-northeast-2.rds.amazonaws.com:3306/timeline", "admin", "maria1234"); // 마리아 DB
 
        System.out.println(con);
 
-   }*/
+   }
 }
