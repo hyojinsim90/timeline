23207e15ed838bed165771ad2230fa593bd4889f
jumi1234 <farewellsad89@gmail.com>
2021-05-24 15:13:45 +0900

    내 정보 수정 페이지 닉네임 중복 체크 및 비밀번호 변경 완료

--- frontend/src/components/MyPage/MyInfo.js
+++ frontend/src/components/MyPage/MyInfo.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import { Input, Button } from 'antd'
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import { Form, Input, Button } from "antd"
 import { useSelector } from "react-redux"
 import Axios from "axios"
 
@@ -9,21 +9,32 @@ const InfoDiv = styled.div`
   form {
     width: 320px;
     display: inline-block;
+    .ant-form-item {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      .ant-form-item-label {
+        text-align: center;
+      }
+    }
+    // 닉네임 영역
+    #nickname {
+      display: flex;
+    }
     label {
       margin-bottom: 1rem;
     }
-    div {
-      width: 100%;
-      display: flex;
-      button {
-        margin-left: 10px;
+    .ant-input-password {
+      height: 32px;
+      input {
+        height: 100%;
       }
     }
-    input {
-      margin-bottom: 1.5rem;
-      &[type=submit] {
+    .ant-form-item:last-child {
+      button {
+        width: 100%;
         background: black;
-        color: white;
+        color: #ffffff;
         margin-top: 1rem;
       }
     }
@@ -34,60 +45,137 @@ const InfoDiv = styled.div`
 const MyInfo = () => {
 
   const [email, setEmail] = useState("")
-  const [nicknamelist, setNicknameList] = useState([])
   const [nickname, setNickname] = useState([])
+  const [password1, setPassword1] = useState("")
+  const [password2, setPassword2] = useState("")
+  const [existingPw, setExistingPw] = useState("")
+  const [checkDuplicate, setCheckDuplicate] = useState(false)
+  const [checkValid, setCheckValid] = useState(true)
+
   const user = useSelector(state => state.user.userData)
 
   useEffect(() => {
     if(user !== undefined) {
       setEmail(user.email)
-    }
-    Axios.get("/member/ids")
-      .then(res => {
-        res.data.forEach((item, i) => {
-          setNicknameList(item.nickname)
+      setNickname(user.nickname)
+
+      Axios.get(`/member/${user.email}`)
+        .then(res => {
+          setExistingPw(res.data.password)
         })
-      })
+    }
   }, [user])
 
   const onChangeNick = (e) => {
     setNickname(e.target.value)
   }
 
+  const onChangePwd1 = (e) => {
+    setPassword1(e.target.value)
+  }
+
+  const onChangePwd2 = (e) => {
+    setPassword2(e.target.value)
+  }
+
   // 중복확인 체크
   const onCheckDup = () => {
+    Axios.get("/member/ids")
+      .then(res => {
+        const duplicate = res.data.filter(item => item.nickname === nickname)
+        // 닉네임 중복일 경우
+        if(duplicate.length > 0) {
+          // 다른 사람 닉네임과 중복일 경우
+          if(nickname !== user.nickname) {
+            setCheckDuplicate(false)
+            alert("이미 있는 닉네임입니다")
+          // 내 기존 닉네임일 경우
+          } else {
+            setCheckDuplicate(true)
+            alert("사용할 수 있는 닉네임입니다")
+          }
+        } else {
+          setCheckDuplicate(true)
+          alert("사용할 수 있는 닉네임입니다")
+        }
+      })
+  }
 
+  const onChangeInfo = () => {
+
+      // 닉네임 중복확인 버튼 클릭하도록 하기
+    if(checkDuplicate === false) {
+      alert("닉네임 중복확인을 해주세요")
+      return false
+    } else if(password1 || password2) {
+      if(password1 !== password2) {
+        alert("비밀번호와 비밀번호 확인이 일치하지 않습니다")
+        return false
+      }
+    }
+
+    const variables = {
+      "nickname": nickname,
+      "password": password1 ? password1 : existingPw
+    }
+
+    if(checkDuplicate && checkValid) {
+      Axios.put(`/member/${email}`, variables)
+        .then(res => {
+          // 닉네임, 비밀번호 변경 성공 시 페이지 새로고침
+          if(res.status === 200) {
+            window.location.reload()
+          }
+        })
+    }
   }
 
   return (
     <InfoDiv>
-      <form>
-        <label>이메일 주소:</label>
-        <Input
-          type='email'
-          value={email}
-          disabled
-        />
-        <label>닉네임:</label><br/>
-        <div>
-          <Input
-            type="text"
-            value={nickname}
-            onChange={onChangeNick}
-            required
-          />
-          <Button onClick={onCheckDup}>중복확인</Button>
-        </div>
-        <label>비밀번호:</label>
-        <Input
-          type='text'
-        />
-        <label>비밀번호 확인:</label>
-        <Input
-          type='text'
-        />
-      <Input type='submit' size="large" value='수정하기' />
-      </form>
+      <Form>
+        <Form.Item
+          label="이메일:"
+          name="email"
+          rules={[{ required: true }]}
+        >
+          <div>
+            { email &&
+              <Input value={email} disabled />
+            }
+          </div>
+        </Form.Item>
+        <Form.Item
+          label="닉네임:"
+          name="nickname"
+          rules={[{ required: true }]}
+        >
+          <div>
+            <Input value={nickname} onChange={onChangeNick} />
+            <Button onClick={onCheckDup}>중복확인</Button>
+          </div>
+        </Form.Item>
+        <Form.Item
+          label="비밀번호(8~16자):"
+          name="password1"
+          rules={[{ required: true, min: 8, max: 16, message: '다시 입력해 주세요' }]}
+        >
+          <Input.Password value={password1} onChange={onChangePwd1} />
+        </Form.Item>
+        <Form.Item
+          label="비밀번호 확인(8~16자):"
+          name="password2"
+          rules={[{ required: true, min: 8, max: 16, message: '다시 입력해 주세요' }]}
+        >
+          <Input.Password value={password2} onChange={onChangePwd2} />
+        </Form.Item>
+        <Form.Item
+          name="button"
+        >
+          <Button size="large" onClick={onChangeInfo}>
+            수정하기
+          </Button>
+        </Form.Item>
+      </Form>
     </InfoDiv>
   )
 }
