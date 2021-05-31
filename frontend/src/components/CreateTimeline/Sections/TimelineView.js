40d76fec1b253c5096edb15cc03d2d19eac8c03a
jumi1234 <farewellsad89@gmail.com>
2021-05-31 11:48:50 +0900

    createtimeline 페이지 detail/view column=>row 정렬

--- frontend/src/components/CreateTimeline/Sections/TimelineView.js
+++ frontend/src/components/CreateTimeline/Sections/TimelineView.js
@@ -1,15 +1,28 @@
 import React from "react"
 import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react'
+import styled from "styled-components"
+
+const TimelineViewDiv = styled.div`
+  .body-container p {
+    word-break: break-all;
+  }
+`
 
 const TimelineView = (props) => {
   return (
-    <>
+    <TimelineViewDiv>
       <Timeline lineColor={'black'}>
         {props.countList && props.countList.map((item, i) => (
           <TimelineItem
             key={i}
             dateText={props.detailDateString[i]}
-            style={{ color: '#e86971' }}
+            style={{ color: '#e86971', height: '495px', }}
+            bodyContainerStyle={{
+              background: '#ddd',
+              padding: '20px',
+              borderRadius: '8px',
+              boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+            }}
           >
             <h3>{props.detailTitle[i]}</h3>
             <p>
@@ -18,7 +31,7 @@ const TimelineView = (props) => {
           </TimelineItem>
         ))}
       </Timeline>
-    </>
+    </TimelineViewDiv>
   )
 }
 
