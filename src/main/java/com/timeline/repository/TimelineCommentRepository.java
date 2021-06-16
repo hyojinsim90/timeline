aced324b27bb0bbd7017e49dd256ced7be761a03
hyojinsim <hyojinsim90@gmail.com>
2021-06-17 03:14:37 +0900

    add timeline like and comment

--- src/main/java/com/timeline/repository/TimelineCommentRepository.java
+++ src/main/java/com/timeline/repository/TimelineCommentRepository.java
@@ -17,6 +17,6 @@ import org.springframework.stereotype.Repository;
 public interface TimelineCommentRepository extends JpaRepository<TimelineComment, Long> {
 
     @Query("SELECT m FROM TimelineComment m WHERE m.masterId = :masterId AND m.nickname = :nickname")
-    TimelineComment findExistOne(@Param("masterId") Long masterId, @Param("memberId") String nickname);
+    TimelineComment findExistOne(@Param("masterId") Long masterId, @Param("nickname") String nickname);
 
 }
